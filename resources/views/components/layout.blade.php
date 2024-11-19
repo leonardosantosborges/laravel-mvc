363a4c0e0da4b44dc9a228b787d8b8b8b442ea36
leonardosantosborges <leonardo.borges@sptech.school>
2024-11-19 15:14:40 -0300

    laravel mvc project

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </meta>
+    </meta>
+    <title>Series</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<div class="container">
+    <h1>{{ $title }} - Controle de Séries</h1>
+
+    {{ $slot }}
+</div>
+</body>
+</html>
